3ef1d4bbc7dbaea0d0a134739ac4056f41ff94a6
Antoine <antoine.devillers01@gmail.com>
2024-09-24 14:45:18 +0200

    modif trips router

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -5,17 +5,15 @@ const Trip = require('../models/trip');
 
 /* GET trips listing. */
 router.get('/:departure/:arrival/:date', (req, res) =>{
-  Trip.find({departure: req.params.departure, arrival: req.params.arrival, date: req.params.date}).then(data=> {
-    console.log('AllTrips =>', data)
-  
+  Trip.find({departure: req.params.departure, arrival: req.params.arrival, date: req.params.date})
+  .then(data=> {
+      console.log('AllTrips =>', data)
 
-  console.log(data)
-
-
-
-  res.json(data);
+    res.json(data);
   })
 });
+
+
 
 /* GET trips sélectionnés + price quand on est dans panier*/
 
