b40b921478fb4521f6c0d320beec30ac72a90500
Your name <Your email>
2024-09-24 13:38:21 +0200

    debut du get pour les voyages sélectionnés

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -11,4 +11,14 @@ router.get('/:departure/:arrival/:date', function(req, res) {
   res.send('respond with a resource');
 });
 
+/* GET trips sélectionnés + price quand on est dans panier*/
+
+router.get('/', (req, res) => {
+
+  res.json();
+});
+/* POST des trips de panier dans booking */
+
+/* DELETE des trips non sélectionnés ou */
+
 module.exports = router;
