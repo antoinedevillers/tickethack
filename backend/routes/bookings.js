96ea7b177ae5a10e190ca7f468fd6fad9d4eefc1
Antoine <antoine.devillers01@gmail.com>
2024-09-24 15:50:59 +0200

    ajout rout post et delete sur booking.js

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -14,6 +14,42 @@ router.get('/', (req, res) => {
 
   /* POST des trips de panier dans booking */
   
-  /* DELETE des trips non sélectionnés ou */
+  router.post('/', (req, res) => {
+
+    //  const timeLeftCalculated = (Date.parse(Number(req.body.date)) - Date.now) /60000
+
+      const newBooking = new Booking({
   
+        departure: req.body.departure,
+        arrival: req.body.arrival,
+        date: req.body.date,
+        timeLeftBeforeDeparture: req.body.timeLeftBeforeDeparture,
+        price: req.body.price,
+        isBooked: false
+
+      })
+      newBooking.save().then(() => {
+ 
+      Booking.find().then(newBooking => {
+        console.log(newBooking);
+      });
+     
+      res.json({ result: true, booking: newBooking })
+    })
+    
+  })
+  /* DELETE des trips non sélectionnés ou */
+  router.delete("/:id", (req, res) => {
+    Booking.deleteOne({_id: req.params.id}).then(deletedDoc => {
+      if (deletedDoc.deletedCount > 0) {
+        // document successfully deleted
+        console.log(deletedDoc)
+        Booking.find().then(data => {
+          res.json({ result: true, Booking: data });
+        });
+      } else {
+        res.json({ result: false, error: "Booking not found" });
+      }
+    });
+  });
 module.exports = router;
