f3ff88649ee0be8a35130215618b91948c297ada
Antoine <antoine.devillers01@gmail.com>
2024-09-25 12:13:30 +0200

    modif script.js bouton book

--- frontend/script.js
+++ frontend/script.js
@@ -17,11 +17,11 @@ fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
                
 				document.querySelector('#resultat').innerHTML += `
                 <div class='container-trip'>
-                    <div class= 'departureCity'>${data[i].departure}</div>
-                    <div class ='arrivalCity'>${data[i].arrival}</div>
-                    <div class ='timeDeparture'>${data[i].date}</div>
-                    <div class='priceTrip'>${data[i].price}</div>
-                    <button type='button' href='card.html' id='${data[i].id}'>Book</button>
+                    <p class= 'departureCity'>${data[i].departure}</p>
+                    <p class ='arrivalCity'>${data[i].arrival}</p>
+                    <p class ='timeDeparture'>${data[i].date}</p>
+                    <p class='priceTrip'>${data[i].price}</p>
+                    <button type='button' class ='buttonBook' href='card.html' id='${data[i]._id}'>Book</button>
                 </div>`;
               //  console.log(data[i]._id)
                 
@@ -29,34 +29,7 @@ fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
 
         }         
             console.log(data._id)
-        document.getElementById(data._id).addEventListener('click', function () {
-            const departure = document.querySelector('#departureInput').textContent;
-            const arrival = document.querySelector('#arrivalInput').textContent;
-            const date = document.querySelector('#dateInput').texContent;
-            const price = document.querySelector('#dateInput').textContent;
         
-            console.log(data._id)
-            fetch('http://localhost:3000/bookings', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({departure, arrival, date, price}),
-            }).then(response => response.json())
-                .then(data => {
-                    if (data.result) {
-                        
-                        document.querySelector('#lisCart').innerHTML += `
-                        <div class='container-trip'>
-                            <div class= 'departureCity'>${data.departure}</div>
-                            <div class ='arrivalCity'>${data.arrival}</div>
-                            <div class ='timeDeparture'>${data.date}</div>
-                            <div class='priceTrip'>${data.price}</div>
-                            <div id='${data.id} >X</div>
-                        </div>`;
-                        
-                    }
-        
-                });
-        });
         
         
         //Quand on clique, on ajoute un trip dans notre panier
@@ -64,7 +37,27 @@ fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
     
 	})
     
+    
 });
+
+
+for (let i = 0; i < document.querySelectorAll('.buttonBook').length; i++) {
+		document.querySelectorAll('.buttonBook')[i].addEventListener('click', function () {
+
+    window.location.assign('card.html')
+           
+    fetch(`http://localhost:3000/bookings/`, {
+
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({id: id}),
+    }).then(response => response.json())
+        .then(data => {
+           console.log(data)
+
+        });
+});
+}
 //Quand on clique, on ajoute un trip dans notre panier
 // document.querySelector('#???').addEventListener('click', function () {
 //     const departure = document.querySelector('#departureInput').textContent;
