6854e7310e6f7aa48d7bd00037e404a167950d07
Antoine <antoine.devillers01@gmail.com>
2024-09-25 16:11:02 +0200

    ajout css

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@
 // var fomatted_date = moment(date).format('YYYY-MM-DD');
 
 //Click sur search => la liste des trips s'affiche
-/*document.getElementById('search').addEventListener('click', function(){
+document.getElementById('search').addEventListener('click', function(){
    const departure = document.querySelector('#departureInput').value;
    const arrival = document.querySelector('#arrivalInput').value;
    const date = document.querySelector('#dateInput').value;
@@ -20,7 +20,7 @@ fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
                     <p class= 'departureCity'>${data[i].departure}</p>
                     <p class ='arrivalCity'>${data[i].arrival}</p>
                     <p class ='timeDeparture'>${data[i].date}</p>
-                    <p class='priceTrip'>${data[i].price}</p>
+                    <p class='priceTrip'>${data[i].price}€</p>
                     <button type='button' class ='buttonBook' href='card.html' id='${data[i]._id}'>Book</button>
                 </div>`;
               //  console.log(data[i]._id)
@@ -42,15 +42,15 @@ fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
 
 
 for (let i = 0; i < document.querySelectorAll('.buttonBook').length; i++) {
-		document.querySelectorAll('.buttonBook')[i].addEventListener('click', function () {
-
+		document.querySelectorAll('.buttonBook')[i].addEventListener('click', function (event) {
+            const eventId= event.target.id
     window.location.assign('card.html')
            
     fetch(`http://localhost:3000/bookings/`, {
 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({id: id}),
+        body: JSON.stringify({tripId}),
     }).then(response => response.json())
         .then(data => {
            console.log(data)
@@ -58,34 +58,11 @@ for (let i = 0; i < document.querySelectorAll('.buttonBook').length; i++) {
         });
 });
 }
-});*/
-//Quand on clique, on ajoute un trip dans notre panier
-// document.querySelector('#???').addEventListener('click', function () {
-//     const departure = document.querySelector('#departureInput').textContent;
-//     const arrival = document.querySelector('#arrivalInput').textContent;
-//     const date = document.querySelector('#dateInput').texContent;
-//     const price = document.querySelector('#dateInput').textContent;
 
 
-// 	fetch('http://localhost:3000/bookings', {
-// 		method: 'POST',
-// 		headers: { 'Content-Type': 'application/json' },
-// 		body: JSON.stringify({departure, arrival, date, price}),
-// 	}).then(response => response.json())
-// 		.then(data => {
-// 			if (data.result) {
 
-// 				document.querySelector('#lisCart').innerHTML += `
-//                 <div class='container-trip'>
-//                     <div class= 'departureCity'>${data[i].departure}</div>
-//                     <div class ='arrivalCity'>${data[i].arrival}</div>
-//                     <div class ='timeDeparture'>${data[i].date}</div>
-//                     <div class='priceTrip'>${data[i].price}</div>
-//                     <div id='${data[i].id} >X</div>
-//                 </div>`;
-// 			}
 
-// 		});
+
 // });// affichage de tous les trajets choisis
 
 fetch('http://localhost:3000/bookings/')
